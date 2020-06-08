ee3a0d78c5ea92eddedadacaf41e3379e6888479
wholesomewilson <wilsonwan88@gmail.com>
2020-06-08 23:19:47 +0800

    added method to check if timeslot is taken

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,16 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :room
   belongs_to :timeslot
+  validates :start_date, presence: true
+
+  def is_not_taken
+    @bookings = Room.find(room_id).bookings.where(start_date: start_date)
+    @bookings.each do |booking|
+      if booking.timeslot.id == timeslot_id.to_i
+        errors.add(:timeslot, "is taken")
+        return false
+      end
+    end
+    return true
+  end
 end
